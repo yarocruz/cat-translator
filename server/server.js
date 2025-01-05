a59d493f2f25055d45abcd61a9b1925db32d819c
yarocruz <yarocruz@gmail.com>
2025-01-05 13:12:25 -0500

    getting app ready to deploy to render

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('../public')); // Serve your frontend files (HTML, CSS, JS)
 
